d37dd683869ecb1e852d469bf258e8cd1f476d54
Jaakko Lind <jaakko.lind@tuni.fi>
2022-12-10 14:59:46 +0200

    Pushed unfinished changes to repo

--- src/tests/jest_test.test.js
+++ src/tests/jest_test.test.js
@@ -1,7 +1,8 @@
-const sum = require('../test_assets/sum')
+//const add = require('../add.js')
+import add from '../add.js'
 
 describe('Testing the Jest framework', () => {
   test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3)
+    expect(add(1, 2)).toBe(3)
   })
 })
