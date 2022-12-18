c584da3000f6ebd024cacfc6994f6f996d8efdcb
Jaakko Lind <jaakko.lind@tuni.fi>
2022-12-18 20:20:41 +0200

    Add test updated according to main branch

--- tests/add.test.js
+++ tests/add.test.js
@@ -1,11 +1,23 @@
 import add from '../src/add'
 
-describe('Tests for add function', () => {
-    test('Adding integers', () => {
+describe('Testing add.js', () => {
+    test('adds 1 + 2 to equal 3', () => {
         expect(add(1, 2)).toBe(3)
     })
 
-    test('Adding floating point numbers', () => {
-        expect(add(1.11, 2.22)).toBe(3.33)
+    test('addition of zeros to equal 0', () => {
+        expect(add(0, 0)).toBe(0)
+    })
+
+    test('addition of positive and negative numbers', () => {
+        expect(add(1, -2)).toBe(-1)
+    })
+
+    test('addition of negative and positive numbers', () => {
+        expect(add(-1, 2)).toBe(1)
+    })
+
+    test('addition of positive number and a float', () => {
+        expect(add(1, 2.4)).toBe(3.4)
     })
 })
